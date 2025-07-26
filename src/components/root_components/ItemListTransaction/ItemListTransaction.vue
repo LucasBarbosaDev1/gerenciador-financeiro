b361ab4f60ab031f82b7a7efcc13aea401e88125
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-07-26 12:46:39 -0300

    feat: itemsListTransaction finalizado e implementado na view transações

---
+++ src/components/root_components/ItemListTransaction/ItemListTransaction.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="item">
+    <div class="item_infos">
+
+      <div class="item_icon"
+        :style="{
+          backgroundColor: iconColor
+        }"
+      >
+        <v-icon>{{ icon }}</v-icon>
+      </div>
+
+      <div class="item_title">
+        <h3>{{ title }}</h3>
+
+        <span>{{ category }} • {{ account }}</span>
+      </div>
+    </div>
+
+    <div class="item_value">
+      <div class="value">
+
+        <h3
+          :style="{
+            color: valueColor
+          }"
+        >R$ {{ value }}</h3>
+
+        <span>{{ date }}</span>
+      </div>
+
+      <div class="item_btn">
+        <button class="btn_edit"
+          @click="emitBtnEditTransation"
+        >
+          <v-icon>mdi-square-edit-outline</v-icon>
+        </button>
+      
+        <button class="btn_del"
+          @click="emitBtnDeleteTransation"
+        >
+          <v-icon>mdi-trash-can-outline</v-icon>
+        </button>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./ItemListTransaction.scss" lang="scss" scoped></style>
+
+<script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue';
+
+  const props = defineProps({
+    icon: {
+      type: String,
+      required: true
+    },
+    iconColor: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    account: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: [Number, String],
+      required: true
+    },
+    valueColor: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    }
+  });
+
+  const emit = defineEmits(['btnEditTransation', 'btnDeleteTransation']);
+
+  const emitBtnEditTransation = () => {
+    emit('btnEditTransation');
+  };
+
+  const emitBtnDeleteTransation = () => {
+    emit('btnDeleteTransation');
+  };
+</script>
--- src/views/Transacoes/Transacoes.vue
+++ src/views/Transacoes/Transacoes.vue
@@ -8,14 +8,55 @@
     @click="newTransaction"
   />
 
+  <div class="list_trasactions">
+    <div class="title_list">
+      <h2>Lista de Transações</h2>
+    </div>
+
+    <div class="container_list">
+      <ItemListTransaction
+        icon="mdi-arrow-top-right"
+        icon-color="var(--v-success_color)"
+        title="Salário"
+        category="Receita"
+        account="Nubank"
+        value="2500,00"
+        value-color="var(--v-success_color)"
+        date="26/07/2025"
+        @btn-edit-transation="btnEditTransation"
+        @btn-delete-transation="btnDeleteTransation"
+      />
+
+      <ItemListTransaction
+        icon="mdi-arrow-bottom-right"
+        icon-color="var(--v-danger_color)"
+        title="Passagens"
+        category="Despesa"
+        account="Nubank"
+        value="160,00"
+        value-color="var(--v-danger_color)"
+        date="15/07/2025"
+        @btn-edit-transation="btnEditTransation"
+        @btn-delete-transation="btnDeleteTransation"
+      />
+    </div>
+  </div>
+
 </template>
 
 <style src="./Transacoes.scss" lang="scss" scoped></style>
 
 <script setup lang="ts">
   import HeaderPages from '@/components/root_components/HeaderPages/HeaderPages.vue';
-  import newTransaction from '../../composables/newTransaction.ts';
+  import newTransaction from '@/composables/newTransaction.ts';
+  import ItemListTransaction from '@/components/root_components/ItemListTransaction/ItemListTransaction.vue';
 
+  // botoes de editar/deletar transação
+  const btnEditTransation = () => {
+    console.log("Botão editar transação");
+  };
 
-
+  const btnDeleteTransation = () => {
+    console.log("Botão deletar transação");
+  };
 </script>

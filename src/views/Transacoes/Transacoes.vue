b023fb1b5bfa6f91c488366c40628d9d6e2b4215
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-08-09 14:16:18 -0300

    feat: front dos filtros da view transacoes finalizado

--- src/views/Transacoes/Transacoes.vue
+++ src/views/Transacoes/Transacoes.vue
@@ -8,6 +8,33 @@
     @click="newTransaction"
   />
 
+  <div class="filter_transaction">
+    <v-autocomplete
+      class="select_custom"
+      clearable
+      density="comfortable"
+      label="Buscar transações..."
+      :items=transactions_name
+    ></v-autocomplete>
+
+
+    <v-select
+      class="select_custom"
+      clearable
+      density="comfortable"
+      label="Todas as categorias"
+      :items=categories
+    ></v-select>
+
+    <v-select
+      class="select_custom" 
+      clearable
+      density="comfortable"
+      label="Todos os tipos"
+      :items=types
+    ></v-select>
+  </div>
+
   <div class="list_trasactions">
     <div class="title_list">
       <h2>Lista de Transações</h2>
@@ -44,12 +71,18 @@
 
 </template>
 
-<style src="./Transacoes.scss" lang="scss" scoped></style>
+<style src="./Transacoes.scss" lang="scss">
+</style>
 
 <script setup lang="ts">
   import HeaderPages from '@/components/root_components/HeaderPages/HeaderPages.vue';
   import newTransaction from '@/composables/newTransaction.ts';
   import ItemListTransaction from '@/components/root_components/ItemListTransaction/ItemListTransaction.vue';
+
+  // lista dos filtros
+  const transactions_name = ['Salário', 'Passagens'];
+  const categories = ['Alimentação', 'Transporte'];
+  const types = ['Despesas', 'Receitas']
 
   // botoes de editar/deletar transação
   const btnEditTransation = () => {

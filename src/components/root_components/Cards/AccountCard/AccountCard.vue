bc8ef3561770118f839f54eb28cf26708f625d2e
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-08-10 15:59:48 -0300

    feat: AccountCards finalizados e implementados na view Contas

---
+++ src/components/root_components/Cards/AccountCard/AccountCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="card">
+
+    <div class="card_header">
+      <div class="card_description">
+      
+        <div class="icon_card"
+          :style="{
+            backgroundColor: backgroundIcon
+          }"
+        >
+          <v-icon>{{ icon }}</v-icon>
+        </div>
+        
+        <div class="infos_card">
+          <h3>{{ title }}</h3>
+          <h4>{{ account }}</h4>
+          <span>{{ type }}</span>
+        </div>
+      </div>
+
+      <div class="card_btn">
+        <button class="btn_edit"
+          @click="emitBtnEditCategory"
+        >
+          <v-icon>mdi-square-edit-outline</v-icon>
+        </button>
+      
+        <button class="btn_del"
+          @click="emitBtnDeleteCategory"
+        >
+          <v-icon>mdi-trash-can-outline</v-icon>
+        </button>
+      </div>
+    </div>
+
+    <div class="card_value">
+      <div class="div_value">
+        <span>Saldo Atual: </span>
+        <strong>{{ current_balance }}</strong>
+      </div>
+
+      <div class="div_value">
+        <span>Saldo Inicial: </span>
+        <span>{{ initial_balance }}</span>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style src="./AccountCard.scss" lang="scss" scoped></style>
+
+<script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue';
+
+  const props = defineProps({
+    backgroundIcon: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    account: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    current_balance: {
+      type: [String, Number],
+      required: true
+    },
+    initial_balance: {
+      type: [String, Number],
+      required: true
+    }
+  });
+
+  // emissao dos eventos do botoes editar/deletar
+  const emit = defineEmits(['btnEditCategory', 'btnDeleteCategory']);
+
+  const emitBtnEditCategory = () => {
+    emit('btnEditCategory');
+    
+  };
+
+  const emitBtnDeleteCategory = () => {
+    emit('btnDeleteCategory');
+  }
+
+</script>
--- src/views/Contas/Contas.vue
+++ src/views/Contas/Contas.vue
@@ -32,18 +32,53 @@
     />
   </div>
 
+  <div class="account_container">
+
+    <div class="title">
+      <h2>Suas Contas</h2>
+    </div>
+
+    <div class="container_cards">
+      <AccountCard 
+        background-icon="var(--v-success_color)"
+        icon="mdi-cash-plus"
+        title="Conta Corrente"
+        account="Nubank"
+        type="Poupança"
+        current_balance="R$ 1.200,00"
+        initial_balance="R$ 0,00"
+        @btn-edit-category="btnEditCategory"
+        @btn-delete-category="btnDeleteCategory"
+      />
+    </div>
+
+  </div>
+
 </template>
 
 
 <style src="./Contas.scss" lang="scss" scoped></style>
 
 <script setup lang="ts">
+  import AccountCard from '@/components/root_components/Cards/AccountCard/AccountCard.vue';
   import MetricCard from '@/components/root_components/Cards/MetricCard/MetricCard.vue';
-import HeaderPages from '@/components/root_components/HeaderPages/HeaderPages.vue';
+  import HeaderPages from '@/components/root_components/HeaderPages/HeaderPages.vue';
 
   const btnEvent = () => {
     console.log('Botão Nova conta');
     
   }
+
+  // botoes de editar/deletar contas
+  const btnEditCategory = () => {
+    console.log('Botao editar conta');
+    
+  }
+
+  const btnDeleteCategory = () => {
+    console.log('Botao deletar conta');
+    
+  }
+
 
 </script>
